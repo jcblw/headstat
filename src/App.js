46d13087f77d08ff768e039f59420e814e32000f
jcblw <jacoblowe2.0@gmail.com>
2017-07-29 15:41:14 -0700

    add in some boilerplate stuff to get it working

---
+++ src/App.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { sdk } from './lib/headspace-sdk';
+import {
+  BarChart,
+  Bar,
+  Legend,
+  Tooltip,
+  YAxis,
+  XAxis,
+  CartesianGrid,
+} from 'recharts';
+
+const CHART_COLOR = '#f58b44';
+
+class App extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {};
+  }
+
+  aggregate(data, formatTitle) {
+    return Object.keys(data)
+      .map(date => {
+        const firstDate = data[date][0]
+          ? data[date][0].updatedAt
+          : data[date].date;
+        return {
+          name: formatTitle(firstDate),
+          sessions: data[date].length,
+          sortBy: firstDate,
+        };
+      })
+      .sort((prev, next) => {
+        return (
+          new Date(prev.sortBy).valueOf() - new Date(next.sortBy).valueOf()
+        );
+      });
+  }
+
+  componentDidMount() {}
+
+  getMonthString(_date) {
+    if (!_date) return 'Invalid Date';
+    const date = new Date(_date);
+    const formatter = new Intl.DateTimeFormat('en', { month: 'long' });
+    return formatter.format(date);
+  }
+
+  formatTitle(_date) {
+    if (!_date) return 'Invalid Date';
+    const date = new Date(_date);
+    const formatter = new Intl.DateTimeFormat('en', { month: 'short' });
+    const month = formatter.format(date);
+    return `${month} ${date.getDate() + 1}`;
+  }
+
+  getAggregation() {
+    let daily;
+    let weekly;
+    let monthly;
+
+    sdk
+      .getCompletionsByDay()
+      .then(d => {
+        daily = this.aggregate(d, this.formatTitle);
+        return sdk.getCompletionsByWeek();
+      })
+      .then(w => {
+        weekly = this.aggregate(w, this.formatTitle);
+        return sdk.getCompletionsByMonth();
+      })
+      .then(m => {
+        monthly = this.aggregate(m, this.getMonthString);
+        this.setState({
+          daily,
+          weekly,
+          monthly,
+        });
+      });
+  }
+
+  onChange(e) {
+    sdk.setAuth(e.target.value);
+    this.getAggregation();
+  }
+
+  render() {
+    const { weekly, daily, monthly, authToken } = this.state;
+    return (
+      <div className="App">
+        <h2>Headstats</h2>
+        <label htmlFor="auth-token">Auth Token</label>
+        <input
+          id="auth-token"
+          type="text"
+          value={authToken}
+          onChange={this.onChange.bind(this)}
+        />
+        <h1>Daily Sessions</h1>
+        <BarChart width={window.innerWidth} height={400} data={daily}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Bar type="monotone" dataKey="sessions" fill={CHART_COLOR} />
+        </BarChart>
+
+        <h1>Weekly Sessions</h1>
+        <BarChart width={window.innerWidth} height={400} data={weekly}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Bar type="monotone" dataKey="sessions" fill={CHART_COLOR} />
+        </BarChart>
+
+        <h1>Monthly Sessions</h1>
+        <BarChart width={window.innerWidth} height={400} data={monthly}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Bar type="monotone" dataKey="sessions" fill={CHART_COLOR} />
+        </BarChart>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/lib/headspace-sdk.js
@@ -0,0 +1,216 @@
+const fetch = require('isomorphic-fetch');
+const qs = require('query-string');
+const normalize = require('json-api-normalizer');
+const decodeJWT = require('jwt-decode');
+require('./date-extensions');
+
+class SDK {
+  constructor(baseURL) {
+    this.baseURL = baseURL || 'https://api.prod.headspace.com';
+  }
+
+  request(endpoint, method, data) {
+    const query =
+      method.toUpperCase() === 'GET' ? `?${qs.stringify(data)}` : '';
+    const options = {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: this.token ? `Bearer ${this.token}` : undefined,
+      },
+      body: data && !query ? JSON.stringify(data) : undefined,
+    };
+    return fetch(this.buildURL(`${endpoint}${query}`), options);
+  }
+
+  setAuth(token) {
+    this.token = token;
+    try {
+      this.meta = decodeJWT(token);
+    } catch (e) {
+      this.token = null;
+      this.meta = null;
+    }
+  }
+
+  buildURL(endpoint) {
+    return `${this.baseURL}${endpoint}`;
+  }
+
+  getMonthKey(date) {
+    return `${date.getFullYear()}-${date.getMonth() + 1}`;
+  }
+
+  getDayKey(date) {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  }
+
+  getWeekKey(date) {
+    return `${date.getFullYear()}-${date.getMonth() +
+      1}-${date.getWeekNumber()}`;
+  }
+
+  getCompletionsByMonth() {
+    return this.allUserActivities().then(activities => {
+      const aggregate = {};
+      let firstDate;
+      let lastDate;
+      activities
+        .filter(activity => activity.status === 'COMPLETE')
+        .forEach(activity => {
+          const { updatedAt } = activity;
+          const createdAtDate = new Date(updatedAt);
+          const dateKey = this.getMonthKey(createdAtDate);
+
+          if (!firstDate) {
+            firstDate = createdAtDate;
+          } else {
+            if (createdAtDate.valueOf() < firstDate.valueOf()) {
+              firstDate = createdAtDate;
+            }
+          }
+
+          if (!lastDate) {
+            lastDate = createdAtDate;
+          } else {
+            if (createdAtDate.valueOf() > lastDate.valueOf()) {
+              lastDate = createdAtDate;
+            }
+          }
+
+          if (!aggregate[dateKey]) {
+            aggregate[dateKey] = [];
+          }
+          aggregate[dateKey].push(activity);
+        });
+
+      let fillDate = firstDate;
+      while (this.getMonthKey(fillDate) !== this.getMonthKey(lastDate)) {
+        const key = this.getMonthKey(fillDate);
+        if (!aggregate[key]) {
+          aggregate[key] = [];
+          aggregate[key].date = fillDate;
+        }
+        fillDate.setMonth(fillDate.getMonth() + 1);
+      }
+      return Promise.resolve(aggregate);
+    });
+  }
+
+  getCompletionsByDay() {
+    return this.allUserActivities().then(activities => {
+      const aggregate = {};
+      let firstDate;
+      let lastDate;
+      activities
+        .filter(activity => activity.status === 'COMPLETE')
+        .forEach(activity => {
+          const { updatedAt } = activity;
+          const createdAtDate = new Date(updatedAt);
+          const dateKey = this.getDayKey(createdAtDate);
+
+          if (!firstDate) {
+            firstDate = createdAtDate;
+          } else {
+            if (createdAtDate.valueOf() < firstDate.valueOf()) {
+              firstDate = createdAtDate;
+            }
+          }
+
+          if (!lastDate) {
+            lastDate = createdAtDate;
+          } else {
+            if (createdAtDate.valueOf() > lastDate.valueOf()) {
+              lastDate = createdAtDate;
+            }
+          }
+
+          if (!aggregate[dateKey]) {
+            aggregate[dateKey] = [];
+          }
+          aggregate[dateKey].push(activity);
+        });
+
+      let fillDate = firstDate;
+      while (this.getDayKey(fillDate) !== this.getDayKey(lastDate)) {
+        const key = this.getDayKey(fillDate);
+        if (!aggregate[key]) {
+          aggregate[key] = [];
+        }
+        fillDate.setDate(fillDate.getDate() + 1);
+      }
+      return Promise.resolve(aggregate);
+    });
+  }
+
+  getCompletionsByWeek() {
+    return this.allUserActivities().then(activities => {
+      const aggregate = {};
+      let firstDate;
+      let lastDate;
+      activities
+        .filter(activity => activity.status === 'COMPLETE')
+        .forEach(activity => {
+          const { updatedAt } = activity;
+          const createdAtDate = new Date(updatedAt);
+          const dateKey = this.getWeekKey(createdAtDate);
+
+          if (!firstDate) {
+            firstDate = createdAtDate;
+          } else {
+            if (createdAtDate.valueOf() < firstDate.valueOf()) {
+              firstDate = createdAtDate;
+            }
+          }
+
+          if (!lastDate) {
+            lastDate = createdAtDate;
+          } else {
+            if (createdAtDate.valueOf() > lastDate.valueOf()) {
+              lastDate = createdAtDate;
+            }
+          }
+
+          if (!aggregate[dateKey]) {
+            aggregate[dateKey] = [];
+          }
+          aggregate[dateKey].push(activity);
+        });
+
+      let fillDate = firstDate;
+      while (this.getWeekKey(fillDate) !== this.getWeekKey(lastDate)) {
+        const key = this.getWeekKey(fillDate);
+        if (!aggregate[key]) {
+          aggregate[key] = [];
+        }
+        fillDate.setDate(fillDate.getDate() + 1);
+      }
+
+      return Promise.resolve(aggregate);
+    });
+  }
+
+  allUserActivities() {
+    const query = {
+      limit: -1,
+      userId: this.meta ? this.meta.userId : undefined,
+    };
+    return this.request('/content/user-activities', 'GET', query)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const { userActivities } = normalize.default(data);
+        const activities = Object.keys(userActivities).map(id => {
+          const activity = Object.assign({}, userActivities[id].attributes, {
+            id,
+          });
+          return activity;
+        });
+        return Promise.resolve(activities);
+      });
+  }
+}
+
+module.exports = SDK;
+module.exports.sdk = new SDK();
